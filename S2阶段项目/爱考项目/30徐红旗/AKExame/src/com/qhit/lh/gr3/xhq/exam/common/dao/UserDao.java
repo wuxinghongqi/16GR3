7c308e7f15511742ecbd6e4a201cdd2457fac176
徐红旗 <33657427+wuxinghongqi@users.noreply.github.com>
2018-04-08 09:56:34 +0800

    Add files via upload

---
+++ S2阶段项目/爱考项目/30徐红旗/AKExame/src/com/qhit/lh/gr3/xhq/exam/common/dao/UserDao.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.qhit.lh.gr3.xhq.exam.common.dao;
+
+import com.qhit.lh.gr3.xhq.exam.common.bean.User;
+
+/**
+ * @author admin
+ * 2017年12月27日
+ */
+public interface UserDao {
+	/**
+	 * @return
+	 */
+	public User login(User user); 
+}
